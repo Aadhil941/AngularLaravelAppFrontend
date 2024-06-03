30fa6bf0fd52ca4b9aa6d86049864303e5882a0a
Aadhil941 <aadhil116@gmail.com>
2024-06-03 10:03:58 +0530

    master new changes updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test-app/test/test.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/test-app/test/test.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss'
+})
+export class TestComponent implements OnInit {
+
+  results : any;
+
+  constructor(private _http: HttpClient) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this._http.get<any>(environment.apiBaseURl + 'api/users').subscribe(
+      (data) => {
+        this.results = data.data;
+        console.log('Data received:', data);
+        // Handle the data
+      },
+      (error) => {
+        console.error('Error:', error);
+        // Handle the error
+      },
+      () => {
+        console.log('Request completed');
+        // Handle the completion
+      }
+    );
+  }
+
+}
